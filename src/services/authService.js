9466ef54dee77e4b6829b2247d5a2cadf9ccdd36
Alexander Uzunov <kaialexander.11@gmail.com>
2025-03-24 16:20:22 +0200

    Adding all files
    Adding all files for the SPA Music App.

---
+++ src/services/authService.js
@@ -0,0 +1,28 @@
+
+
+export const saveUser = (user) => {
+
+    if (user.accessToken) {
+        localStorage.setItem('user', JSON.stringify(user));
+    }
+
+};
+
+export const deleteUser = () => {
+    localStorage.removeItem('user');
+};
+
+export const getToken = () => {
+    return getUser()?.accessToken;
+};
+
+export const getUser = () => {
+    let serializedUser = localStorage.getItem('user');
+
+    if (serializedUser) {
+        let user = JSON.parse(serializedUser);
+
+        return user;
+    }
+
+};
---
+++ src/views/logoutView.js
@@ -0,0 +1,9 @@
+//import { html } from '../../node_modules/lit-html/lit-html.js';
+import * as userService from '../services/userService.js';
+
+export const logoutView = (ctx) => {
+    userService.logout()
+        .then(() => {
+            ctx.page.redirect('/');
+        });
+}
